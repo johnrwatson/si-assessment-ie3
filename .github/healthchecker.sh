ef83fecc9daaaba8ca4a99c8d9cdf5e60cb49840
John Watson <john@familywatson.co.uk>
2023-07-08 10:36:06 +0100

    chore: added build in CI

---
+++ .github/healthchecker.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Helper function to run basic http integrity checks on an endpoint, call it like:
+# healthchecker.sh 15 user:pass http://localhost:8080. Stages results in /tmp/url-responses.
+
+# Where:
+# $1: seconds to run the check for
+# $2: basic auth header
+# $3: endpoint to validate for 200 response
+
+endpoint_healthcheck() {
+
+    for url in $URLS
+    do
+
+        LINK_HTTP_CODE=$(curl -u $AUTH_HEADERS -I -s -m 5 -o /dev/null -w "%{http_code}" $url || echo "500" )
+        echo "INFO: $LINK_HTTP_CODE found for $url"
+
+        if [ "$LINK_HTTP_CODE" -eq 200 ]
+        then
+            echo "0" >> /tmp/url-responses
+        else
+            echo "1" >> /tmp/url-responses
+        fi
+
+    done
+
+    FAILURE_NUMBER=$(cat /tmp/url-responses | { grep "1" | wc -l | xargs || true; } )
+    echo "INFO: FAILURE_NUMBER found to be $FAILURE_NUMBER"
+
+    rm /tmp/url-responses
+
+    [[ $FAILURE_NUMBER == 0 ]] && echo "SUCCESS: All endpoints returned 200" && exit 0 || true
+}
+
+main() {
+
+    echo "INFO: Running url checks for $SECS_TO_CHECK seconds."
+
+    COUNT=0
+
+    while [ "$COUNT" -lt $SECS_TO_CHECK ]
+    do
+        endpoint_healthcheck
+        COUNT=$((COUNT+1))
+        sleep 1
+    done
+
+    echo "ERROR: Endpoints did not return 200"
+    exit 1
+
+}
+
+export SECS_TO_CHECK=$1  # < 60 (minutes) >
+export AUTH_HEADERS=$2   # < automation-user:automation-user >
+export URLS="${@:3}"     # < List of URLs >
+
+main $SECS_TO_CHECK $URLS
--- Makefile
+++ Makefile
@@ -4,16 +4,22 @@ SHELL = /bin/sh
 .PHONY: build, test
 default: build
 
+# There is some code duplication here that could be handled later. For now this will
+# function accordingly for quick validation
+
 deploy:
 	docker compose -f ./local-development/docker-compose-stack.yml --env-file ./local-development/development.env up
 
 build-be:
-	echo "Info: Building Backend:"
 	docker build --platform linux/amd64 -f ./backend/hosting/docker/Dockerfile -t si-assessment-ie3-backend:latest .
 
 build-fe:
-	echo "Info: Building Frontend:"
 	docker build --platform linux/amd64 -f ./frontend/hosting/docker/Dockerfile -t si-assessment-ie3-frontend:latest .
 
-fmt:
-	go fmt ./cmd/...
+validate-be:
+	docker compose -f ./local-development/docker-compose-be.yml --env-file ./local-development/development.env up -d
+	.github/healthchecker.sh 15 user:pass http://localhost:3030/api/users
+
+validate-fe:
+	docker compose -f ./local-development/docker-compose-fe.yml --env-file ./local-development/development.env up -d
+	.github/healthchecker.sh 15 user:pass http://localhost:8080
